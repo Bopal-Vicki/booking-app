4ae5f60dbcec55a6c8cbab9da750f9edbcaa90ff
boopalan <email>
2022-01-13 00:16:58 +0530

    changed env variable

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const mongoURL = `mongodb+srv://boopalan:${process.env.DB_PASSWORD}@cluster0.scngs.mongodb.net/booking-app?retryWrites=true&w=majority`;
+const mongoURL = `mongodb+srv://boopalan:bKEJ4peodWR3XOYw@cluster0.scngs.mongodb.net/booking-app?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoURL, () => {});
