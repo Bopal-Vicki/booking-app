265a56e9c8a90cc3b7fc79c3d943df75049d90ff
boopalan <email>
2022-01-12 23:58:59 +0530

    handled error

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const mongoURL = `mongodb+srv://boopalan:${process.env.DB_PASSWORD}@cluster0.scngs.mongodb.net/booking-app?retryWrites=true&w=majority`;
 
