f9594d605859a6f3315cdd3fd0a9902fedadd3a3
boopalan <email>
2022-01-13 00:22:06 +0530

    error

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const mongoURL = `mongodb+srv://boopalan:bKEJ4peodWR3XOYw@cluster0.scngs.mongodb.net/booking-app?retryWrites=true&w=majority`;
-
-mongoose.connect(mongoURL, () => {});
+mongoose.connect(process.env.MONGOURL, () => {
+  console.log("connected");
+});
